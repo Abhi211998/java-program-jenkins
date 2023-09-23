df9d1413e448bf6eb2731f57e5ea2f78c5748b7d
Abhi211998 <abhi.soni211998@gmail.com>
2023-09-23 18:36:03 +0530

    added Student class

--- Main.java
+++ Main.java
@@ -5,5 +5,7 @@ public class Main{
         System.out.println("Hello jenkins");
         System.out.println(new Date());
         System.out.println("Hello");
+        Student st1 = new Student();\
+        st1.sayHello();
     }
 }
