82030acf858be47ab6b62dba8b13ace02012fc30
Abhi211998 <abhi.soni211998@gmail.com>
2023-09-23 18:21:32 +0530

    Initial commit

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.util.Date;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("Hello jenkins");
+        System.out.println(new Date());
     }
 }
